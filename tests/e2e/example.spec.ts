aa074433d64bd15ddc946e4bb4f05208f86195d2
MysteriousMagpie <malachisellsproduce@gmail.com>
2025-06-13 20:43:41 -0700

    chore: remove root configs and update paths

--- tests/e2e/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('homepage has title', async ({ page }) => {
-  await page.goto('http://localhost:8080', { waitUntil: 'domcontentloaded' });
-
-  const expectedTitle = process.env.PAGE_TITLE || 'life-flow-ai-coach';
-await expect(page).toHaveTitle(expectedTitle);
-});
